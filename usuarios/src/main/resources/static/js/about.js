d39e3df5c3a855900a06aca079b5d0074c0771fd
Bruno Cruz Granados <bruno@brus2099.(none)>
2023-04-20 21:41:54 -0500

    About page completed!!

---
+++ usuarios/src/main/resources/static/js/about.js
@@ -0,0 +1,37 @@
+const menu = document.querySelector('.title');
+const yosh = document.querySelector('#yosh');
+const brandon = document.querySelector('#brandon');
+const brus = document.querySelector('#brus');
+
+menu.addEventListener('mouseover', (e) => {
+  menu.classList.add('hovered-main');
+});
+
+
+yosh.addEventListener('mouseover', (e) => {
+  yosh.classList.add('hovered-card');
+  menu.classList.remove('no-hovered-main');
+});
+
+yosh.addEventListener('mouseleave', (e) => {
+  yosh.classList.remove('hovered-card');
+});
+
+brandon.addEventListener('mouseover', (e) => {
+  brandon.classList.add('hovered-card');
+  menu.classList.remove('no-hovered-main');
+});
+
+brandon.addEventListener('mouseleave', (e) => {
+  brandon.classList.remove('hovered-card');
+});
+
+brus.addEventListener('mouseover', (e) => {
+  brus.classList.add('hovered-card');
+  menu.classList.remove('no-hovered-main');
+});
+
+brus.addEventListener('mouseleave', (e) => {
+  brus.classList.remove('hovered-card');
+});
+
